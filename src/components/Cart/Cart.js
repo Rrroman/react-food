d92e050ee82815fc6c732268f2993b1e5e61d1d1
rrroman <romankohutyak@gmail.com>
2023-01-26 14:49:35 +0200

    Feat: add cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classes from './Cart.module.css';
+const DUMMY_LIST = [
+  { id: 'c1', name: 'Sushi', amount: 2, price: 12.99 },
+  { id: 'c2', name: 'Beer', amount: 4, price: 32.99 },
+  { id: 'c3', name: 'Potato', amount: 3, price: 2.99 },
+];
+
+const Cart = () => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {DUMMY_LIST.map((food) => (
+        <li>{food.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount: 10</span>
+        <span>Total Price: 42</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
