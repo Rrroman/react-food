0d86e70215a1d2c9eada4cf355f92204859ea0a4
rrroman <romankohutyak@gmail.com>
2023-02-01 16:16:27 +0200

    Add reducer and default state

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return {
+        ...state,
+        items: [...state.items, action.item],
+        totalAmount: state.totalAmount + action.item.price * action.item.amount,
+      };
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.item.id),
+      };
+    default:
+      return state;
+  }
+};
+
 const CartProvider = (props) => {
-  const addItem = (item) => {};
-  const removeItem = (id) => {};
+  const [cartState, dispatch] = useReducer(cartReducer, defaultCartState);
+  const addItem = (item) => {
+    dispatch({ type: 'ADD_ITEM', item });
+  };
+  const removeItem = (id) => {
+    dispatch({ type: 'REMOVE_ITEM', id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem,
     removeItem,
   };
